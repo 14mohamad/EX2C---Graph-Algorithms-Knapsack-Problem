e884fa920d3d2c06c92725e40b5b401300d87665
Mohamad Mousa <93079380+14mohamad@users.noreply.github.com>
2024-02-11 23:41:02 +0200

    my_Knapsack.c

--- my_Knapsack.c
+++ my_Knapsack.c
@@ -1,78 +1,61 @@
 #include <stdio.h>
 
+#define MAX_ITEMS 5
 #define MAX_WEIGHT 20
-#define ARRAY_SIZE 5
 
-int max(int a, int b) 
-{ 
-    return (a > b)? a : b; 
-}
-
-
-int knapSack(int weights[], int values[], int selected_bool[])
-{
+int knapSack(int weights[], int values[], int selected_bool[]) {
+    int dp[MAX_ITEMS + 1][MAX_WEIGHT + 1];
     int i, w;
-    int K[ARRAY_SIZE + 1][MAX_WEIGHT + 1];
 
-    for (i = 0; i <= ARRAY_SIZE; i++) {
+    // Build DP table
+    for (i = 0; i <= MAX_ITEMS; i++) {
         for (w = 0; w <= MAX_WEIGHT; w++) {
             if (i == 0 || w == 0)
-                K[i][w] = 0;
+                dp[i][w] = 0;
             else if (weights[i - 1] <= w)
-                K[i][w] = max(values[i - 1] + K[i - 1][w - weights[i - 1]], K[i - 1][w]);
+                dp[i][w] = (values[i - 1] + dp[i - 1][w - weights[i - 1]]) > dp[i - 1][w] ? 
+                           (values[i - 1] + dp[i - 1][w - weights[i - 1]]) : dp[i - 1][w];
             else
-                K[i][w] = K[i - 1][w];
+                dp[i][w] = dp[i - 1][w];
         }
     }
 
-    int res = K[ARRAY_SIZE][MAX_WEIGHT];
+    // Trace back to find selected items
+    int res = dp[MAX_ITEMS][MAX_WEIGHT];
     w = MAX_WEIGHT;
-    for (i = ARRAY_SIZE; i > 0 && res > 0; i--) {
-        if (res == K[i - 1][w])
-            continue;
+    for (i = MAX_ITEMS; i > 0 && res > 0; i--) {
+        if (res == dp[i - 1][w])
+            selected_bool[i - 1] = 0;
         else {
             selected_bool[i - 1] = 1;
             res -= values[i - 1];
             w -= weights[i - 1];
         }
     }
-    return K[ARRAY_SIZE][MAX_WEIGHT];
+
+    return dp[MAX_ITEMS][MAX_WEIGHT];
 }
 
-
-
 int main() {
-    char items[] = {'A', 'B', 'C', 'D', 'E'};
-    int i = 0;
-    int values[ARRAY_SIZE] = {0};
-    for ( i = 0; i < ARRAY_SIZE; i++)
-    {
-        printf("Enter number for values %d: ", i + 1);
-        scanf("%d", &values[i]);
+    char items[MAX_ITEMS] = {'A', 'B', 'C', 'D', 'E'};
+    int values[MAX_ITEMS], weights[MAX_ITEMS], selected_bool[MAX_ITEMS] = {0};
+    int i, max_profit;
+
+    printf("Enter the values and weights for each item:\n");
+    for (i = 0; i < MAX_ITEMS; i++) {
+        printf("Enter value and weight for item %c: ", items[i]);
+        scanf("%d %d", &values[i], &weights[i]);
     }
-    printf("\n");
 
-    int weights[ARRAY_SIZE] = {0};
-    for ( i = 0; i < ARRAY_SIZE; i++)
-    {
+    max_profit = knapSack(weights, values, selected_bool);
 
-        printf("Enter number for weights %d: ", i + 1);
-        scanf("%d", &weights[i]);
-    }
-    printf("\n");
-
-
-    int selected_bool[ARRAY_SIZE] = {0}; // Array to track selected items
-    int maxProfit = knapSack(weights, values, selected_bool);
-    printf("Maximum profit: %d\nItems that give the maximum profit: [", maxProfit);
-    
-    int firstItemPrinted = 0;
-    for (int i = ARRAY_SIZE - 1; i >= 0; i--) {
-        if (selected_bool[i]) {
-            if (firstItemPrinted)
-                printf(", ");
+    printf("\nMaximum profit: %d\n", max_profit);
+    printf("Items that give the maximum profit: [");
+    for (i = MAX_ITEMS - 1; i >= 0; i--) {
+        if (selected_bool[i] == 1) {
             printf("%c", items[i]);
-            firstItemPrinted = 1;
+            if (i-1 > 0)
+                printf(", ");
         }
     }
     printf("]\n");
