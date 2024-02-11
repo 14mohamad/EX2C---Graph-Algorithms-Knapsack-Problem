bc559f1ea80f34bc7160c7a9d9b6e7b2598a365c
Mohamad Mousa <93079380+14mohamad@users.noreply.github.com>
2024-02-11 22:25:38 +0200

    my_graph.c

--- my_graph.c
+++ my_graph.c
@@ -5,37 +5,27 @@
 #define N 10
 
 int main(){
-int i, j;
-char x='1';
+    int i, j;
+    char x='1';
 
-while (x !='D'){
-scanf("%c", &x);
-if(x=='A'){
-GetArr();
-ShortestPaths();
+    while (x !='D'){
+        scanf("%c", &x);
+        if(x=='A')
+        {
+            GetArr();
+            ShortestPaths();
+        }
+        else if(x== 'B')
+        {
+            scanf("%d",&i);
+            scanf("%d", &j);
+            printf("%s\n", (mat[i][j] > 0) ? "True" : "False");
+        }
+        else if(x == 'C')
+        {
+            scanf("%d",&i);
+            printf("%d\n", (mat[i][j] == 0) ? -1 : mat[i][j]);
+        }
+    }
+    return 0;
 }
-
-if(x=='B'){
-scanf("%d",&i);
-scanf("%d", &j);
-if(mat[i][j] >0){
-printf("True\n");
-}
-else{
-printf("False\n");
-}
-}
-
-if(x=='C'){
-scanf("%d",&i);
-scanf("%d", &j);
-if(mat[i][j]==0){
-printf("-1\n");
-}
-else{
-printf("%d\n", mat[i][j]);
-}
-}
-}
-}
-
